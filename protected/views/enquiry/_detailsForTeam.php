46141305c996522d1091817a7d30889dd2f1cafe
buttle <chris@gatopelao.org>
2013-10-15 12:02:05 +0200

    _detailsForTeam.php got corrupted

--- protected/views/enquiry/_detailsForTeam.php
+++ protected/views/enquiry/_detailsForTeam.php
@@ -1,22 +1,75 @@
-budget/featured.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
-budget/admin.php:$this->contextHelp=$this->contextHelp.'<p><img src="'.Yii::app()->theme->baseUrl.'/images/update.png" /> Change a budget<br />';
-budget/admin.php:$this->contextHelp=$this->contextHelp.'<img src="'.Yii::app()->theme->baseUrl.'/images/insert_icon.png" /> Create a sub budget</p>';
-budget/admin.php:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
-budget/view.php~:	background-image: url('<?php echo Yii::app()->theme->baseUrl; ?>/images/graph_paper.png');
-budget/_interactive.php~:			echo '<img class="showChildren" src="'.Yii::app()->theme->baseUrl.'/images/plus_icon.png" onClick="js:toggleChildren('.$budget->id.');"/>';
-budget/admin.php~:$this->contextHelp=$this->contextHelp.'<p><img src="'.Yii::app()->theme->baseUrl.'/images/update.png" /> Change a budget<br />';
-budget/admin.php~:$this->contextHelp=$this->contextHelp.'<img src="'.Yii::app()->theme->baseUrl.'/images/insert_icon.png" /> Create a sub budget</p>';
-budget/admin.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
-budget/feature.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
-budget/featured.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
-budget/featured.php:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
-budget/_index.php~:					'<img style="vertical-align:text-bottom;cursor:pointer" src="<?php echo Yii::app()->theme->baseUrl?>/images/go_back.png" '+
-budget/_index.php~:echo '<img style="float:right;" src="'.Yii::app()->theme->baseUrl.'/images/graph_types.png" />';
-budget/adminYears.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
-budget/_interactive.php:			echo '<img class="showChildren" src="'.Yii::app()->theme->baseUrl.'/images/plus_icon.png" onClick="js:toggleChildren('.$budget->id.');"/>';
-budgetDescription/view.php:	'cssFile' => Yii::app()->theme->baseUrl.'/css/pdetailview.css',
-budgetDescription/admin.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
-budgetDescription/_form.php:		'cssFile' => Yii::app()->theme->baseUrl.'/css/pdetailview.css',
-bulkEmail/view.php:echo '<img id="loading" src="'.Yii::app()->theme->baseUrl.'/images/small_loading.gif" style="vertical-align:middle;margin-left:15px;display:none"/>';
-bulkEmail/admin.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
-bulkEmail/view.php~:echo '<img id="loading" src="'.Yi
+<?php
+/**
+ * OCAX -- Citizen driven Municipal Observatory software
+ * Copyright (C) 2013 OCAX Contributors. See AUTHORS.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+	'cssFile' => Yii::app()->request->baseUrl.'/css/pdetailview.css',
+	'data'=>$model,
+	'attributes'=>array(
+		array(
+	        'label'=>__('State'),
+	        'value'=>$model->getHumanStates($model->state),
+		),
+		array(
+	        'label'=>__('Formulated by'),
+	        'value'=>$model->user0->fullname.' '.__('on the').' '.$model->created.' ('.$model->user0->email.')',
+		),
+		array(
+	        'label'=>__('Assigned to'),
+	        'value'=>($model->team_member) ? $model->teamMember->fullname.' '.__('on the').' '.$model->assigned : "",
+		),
+		array(
+	        'label'=>__('Type'),
+	        'value'=>($model->related_to) ? $model->getHumanTypes($model->type).' ('.__('reformulated').')' : $model->getHumanTypes($model->type),
+		),
+		array(
+	        'label'=>__('Subscribed users'),
+	        'value'=>count($model->subscriptions),
+		),
+	),
+));
+
+if($model->state >= ENQUIRY_AWAITING_REPLY){
+	$document=', Doc: ';
+	if($model->documentation)
+		$document .='<a href="'.$model->documentation0->getWebPath().'" target="_new">'.$model->documentation0->name.'</a>';
+	else
+		$document .='<span style="color:red">'.__('missing').'</span>';
+	$submitted_info=$model->submitted.', '.__('Registry number').': '.$model->registry_number.$document;
+
+	$this->widget('zii.widgets.CDetailView', array(
+	'cssFile' => Yii::app()->request->baseUrl.'/css/pdetailview.css',
+	'data'=>$model,
+	'attributes'=>array(
+		array(
+	        'label'=>__('Submitted'),
+			'type'=>'raw',
+	        'value'=>$submitted_info,
+		),
+	),
+	));
+}
+
+if($model->budget){
+	$budget=Budget::model()->findByPk($model->budget);
+	$this->renderPartial('//budget/_enquiryView', array('model'=>$budget,'showMore'=>1));
+}
+
+?>
