4677989ecf7fcc67d66da1f81347b5526a4e6f00
buttle <chris@gatopelao.org>
2013-10-15 11:08:23 +0200

    fixing up style

---
+++ protected/views/enquiry/_detailsForTeam.php
@@ -0,0 +1,22 @@
+budget/featured.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
+budget/admin.php:$this->contextHelp=$this->contextHelp.'<p><img src="'.Yii::app()->theme->baseUrl.'/images/update.png" /> Change a budget<br />';
+budget/admin.php:$this->contextHelp=$this->contextHelp.'<img src="'.Yii::app()->theme->baseUrl.'/images/insert_icon.png" /> Create a sub budget</p>';
+budget/admin.php:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
+budget/view.php~:	background-image: url('<?php echo Yii::app()->theme->baseUrl; ?>/images/graph_paper.png');
+budget/_interactive.php~:			echo '<img class="showChildren" src="'.Yii::app()->theme->baseUrl.'/images/plus_icon.png" onClick="js:toggleChildren('.$budget->id.');"/>';
+budget/admin.php~:$this->contextHelp=$this->contextHelp.'<p><img src="'.Yii::app()->theme->baseUrl.'/images/update.png" /> Change a budget<br />';
+budget/admin.php~:$this->contextHelp=$this->contextHelp.'<img src="'.Yii::app()->theme->baseUrl.'/images/insert_icon.png" /> Create a sub budget</p>';
+budget/admin.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
+budget/feature.php~:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
+budget/featured.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
+budget/featured.php:					'imageUrl' => Yii::app()->theme->baseUrl.'/images/insert_icon.png',
+budget/_index.php~:					'<img style="vertical-align:text-bottom;cursor:pointer" src="<?php echo Yii::app()->theme->baseUrl?>/images/go_back.png" '+
+budget/_index.php~:echo '<img style="float:right;" src="'.Yii::app()->theme->baseUrl.'/images/graph_types.png" />';
+budget/adminYears.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
+budget/_interactive.php:			echo '<img class="showChildren" src="'.Yii::app()->theme->baseUrl.'/images/plus_icon.png" onClick="js:toggleChildren('.$budget->id.');"/>';
+budgetDescription/view.php:	'cssFile' => Yii::app()->theme->baseUrl.'/css/pdetailview.css',
+budgetDescription/admin.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
+budgetDescription/_form.php:		'cssFile' => Yii::app()->theme->baseUrl.'/css/pdetailview.css',
+bulkEmail/view.php:echo '<img id="loading" src="'.Yii::app()->theme->baseUrl.'/images/small_loading.gif" style="vertical-align:middle;margin-left:15px;display:none"/>';
+bulkEmail/admin.php:	'cssFile'=>Yii::app()->theme->baseUrl.'/css/pgridview.css',
+bulkEmail/view.php~:echo '<img id="loading" src="'.Yi
