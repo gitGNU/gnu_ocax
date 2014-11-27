72247987aae7f817ae2e22c37030a8a0d45d8044
buttle <chris@gatopelao.org>
2014-11-27 16:50:13 +0100

    removed old coloured theme directories

--- protected/migrations/migrate-version-0000.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO config(parameter, value, required, description) VALUES ('safeHTMLeditor', '1', '1', "Safe HTML editor. 0 = No, 1 = Yes.");
-INSERT INTO config(parameter, value, required, description) VALUES ('socialActivateNonFree', '0', '1', "Include Twitter and Facebook widgets. 0 = No, 1 = Yes.");
-
--- protected/migrations/migrate-version-0001.sql
+++ protected/migrations/migrate-version-0001.sql
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS log (
   id int(11) NOT NULL AUTO_INCREMENT,
   user int(11) NULL,		/* NULL because some things might be system log */
   created int(11) NOT NULL,	/* unix time stamp */
-  prefix varchar(255) NOT NULL,	/* possible is the name of a model */
+  prefix varchar(255) NOT NULL,	/* possibly is the name of a model */
   model_id int(11) NULL,
   message varchar(1024) NOT NULL,
   PRIMARY KEY (id)
